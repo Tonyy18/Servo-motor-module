b165e744d2d321e2763eb5e0b1e92f7300351666
Tonyy18 <46251776+Tonyy18@users.noreply.github.com>
2018-12-30 02:03:29 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Servo-motor-module
+# Raspberry Pi Servo motor module
+Control servo motor with raspberry pi and python
+
+Uses <a href="https://pypi.org/project/RPi.GPIO/">RPi.GPIO</a> and time libraries.
+
+<h2>Constructor</h2>
+Create new servo instance
+
+```
+import Servo
+
+servo = Servo.Init(7)
+```
+Provide your control/pwm pin in the constructor
+
+<h2>Run()</h2>
+Use this method to control your motor by degree<br />
+Accepts degrees between 0 and 180
+
+```
+import Servo
+
+servo = Servo.Init(7)
+servo.run(90)
+```
---
+++ Servo.py
@@ -0,0 +1,34 @@
+try:
+	import RPi.GPIO as pi
+	from time import sleep
+except:
+	raise Exception("Libraries are missing")
+
+class Init:
+
+	def __init__(self, pin):
+		
+		self.length = 40 #Pulse count / While loop count
+		self.pin = int(pin)
+		pi.setmode(pi.BOARD)
+		pi.setup(self.pin, pi.OUT)
+		pi.output(self.pin, False)
+		
+	def run(self, degree):
+		
+		if degree < 0: degree = 0
+		if degree > 180: degree = 180
+
+		ms = 0.001 / 100 * (degree * 100 / 180)
+		ms = 0.001 + ms
+
+		count = 0
+		while count < self.length:
+		
+			pi.output(self.pin, True)
+			sleep(ms)
+			pi.output(self.pin, False)
+			sleep(0.02 - ms)
+			count = count + 1			
+
+		pi.output(self.pin, False)
